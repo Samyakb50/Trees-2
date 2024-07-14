5f53d18bbbca06a8f08e47f6f4149bbf4f0ae21e
Samyak Jain <samyak.jain@thalesgroup.com>
2024-07-14 18:37:22 +0530

    Added class solution also

--- Sample.java
+++ Sample.java
@@ -24,6 +24,25 @@ class Solution {
         }
     };
     
+    class Solution {
+        int res;
+        private void helper(TreeNode root, int currSum){
+            if (root == null)
+                return;
+            
+            currSum = currSum * 10 + root.val;
+            if (root.left == null && root.right == null)
+                res += currSum;
+            
+            helper(root.left, currSum);
+            helper(root.right, currSum);
+        }
+        public int sumNumbers(TreeNode root) {
+            this.res = 0;
+            helper(root, 0);
+            return this.res;
+        }
+    }
 // Time Complexity : O(n)
 // Space Complexity : O(n)
 // Did this code successfully run on Leetcode : yes
